dc317f05035aeb90e70d11b4d3b0adaf41240eba
Sebastian Lara Menares <slaramen@gmail.com>
2017-09-22 23:51:13 -0300

    Add commands

---
+++ README.md
@@ -0,0 +1,33 @@
+# StarsConfAlert
+
+StarsConfAlert is a Telegram bot aimed to participate in this [contest](https://medium.com/@SynapticSpa/construye-un-bot-para-la-starsconf-y-gana-una-entrada-d16cb0e24143) from Synaptic.
+
+
+
+## Instructions
+
+Follow instructions from [Telegram bots introduction](https://core.telegram.org/bots#3-how-do-i-create-a-bot) to register your bot and get your API key
+
+Then, install your python requirements:
+
+```
+pip install -r requirements.txt
+```
+
+Now you can run the bot with
+
+```
+python starsconfalertbot.py <Your API key>
+```
+
+## Usage
+Current commands are:
+
+```
+/alerttime 5
+/stop
+/restart
+/help
+```
+
+
--- starsconfalertbot.py
+++ starsconfalertbot.py
@@ -7,10 +7,7 @@ from telepot.loop import MessageLoop
 from gql import gql, Client
 from gql.transport.requests import RequestsHTTPTransport
 import pendulum
-
-SECONDS_BEFORE = 5 * 60
-
-chatids = []
+from datetime import timedelta as td
 
 transport = RequestsHTTPTransport('https://api-starsconf.synaptic.cl/graphql.')
 client = Client(transport=transport)
@@ -28,18 +25,91 @@ query = gql('''
 
 ''')
 
+response = client.execute(query)
+
+users = {}
+
+
+helpmsg = '''
+Current commands are:
+
+/alerttime 5    # Alert time in minutes
+/stop           # Stop alerts
+/restart        # Restart alerts
+/help           # show this help
+
+'''
+
+def helpme(chatid, *kw):
+    bot.sendMessage(
+        chatid,
+        helpmsg
+    )
+
+
+def alerttime(chatid, kw):
+    users[chatid][u'alerttime'] = int(kw[0])
+    bot.sendMessage(
+        chatid,
+        'Tiempo cambiado a {} minutos'.format(int(kw[0]))
+    )
+
+
+def stop(chatid, kw):
+    users[chatid][u'active'] = False
+    bot.sendMessage(
+        chatid,
+        'Alertas detenidas'
+    )
+
+
+def restart(chatid, kw):
+    users[chatid][u'active'] = True
+    bot.sendMessage(
+        chatid,
+        'Alertas reiniciadas'
+    )
+
+dispatch = {
+    u'/alerttime': alerttime,
+    u'/restart': restart,
+    u'/stop': stop,
+    u'/help': helpme
+}
+
 
 def handle(msg):
     flavor = telepot.flavor(msg)
     summary = telepot.glance(msg, flavor=flavor)
-    chatids.append(summary[2])
+    chatid = summary[2]
+
+    # first time user
+    if chatid not in users:
+        users[chatid] = {
+            'alerttime': 5,
+            'active': True
+        }
+        helpme(chatid)
+
+    # dispatch commands
+    if msg[u'text'][0] == u'/':
+        commands = msg[u'text'].split()
+        command = commands[0]
+        if command in dispatch:
+            dispatch[command](chatid, commands[1:])
+        else:
+            bot.sendMessage(
+                chatid,
+                'Comando no implementado!'
+            )
+            helpme(chatid)
 
 TOKEN = sys.argv[1]
 bot = telepot.Bot(TOKEN)
 MessageLoop(bot, handle).run_as_thread()
 
-response = client.execute(query)
 
+# parse response and take care of timezones
 talks = []
 for talk in response[u'allTalks']:
     datestr = '{} {}'.format(
@@ -48,17 +118,26 @@ for talk in response[u'allTalks']:
     )
     talks.append({
         u'name': talk[u'name'],
-        u'date': pendulum.parse(datestr, tz='America/Santiago')})
+        u'date': pendulum.parse(datestr, tz='America/Santiago'),
+        u'users': []})
 
 print 'Listening ...'
 while True:
     now = pendulum.now()
-    for talk in talks:
-        if (talk['date'] - now).total_seconds() < SECONDS_BEFORE:
-            for chatid in chatids:
+    for chatid in users.keys():
+        user = users[chatid]
+        for talk in talks:
+            minutes_to_talk = (talk[u'date'] - now).total_seconds() / 60.
+
+            if (minutes_to_talk < user[u'alerttime'] and
+                    chatid not in talk[u'users']):
                 bot.sendMessage(
                     chatid,
-                    '{} está por comenzar'.format(talk[u'name'])
+                    '{} está a {} minutos de comenzar!'.format(
+                        talk[u'name'],
+                        user[u'alerttime']
+                    )
                 )
-            talks.remove(talk)
+                # user already alerted
+                talk[u'users'].append(chatid)
     time.sleep(10)
