ece50a664f6dca3ad4762cd63251123ee4493656
Sebastian Lara Menares <slaramen@gmail.com>
2017-10-22 23:00:05 -0300

    Fix users set

--- starsconfalertbot.py
+++ starsconfalertbot.py
@@ -8,6 +8,7 @@ from gql import gql, Client
 from gql.transport.requests import RequestsHTTPTransport
 import pendulum
 from datetime import timedelta as td
+from pprint import pprint
 
 transport = RequestsHTTPTransport('https://api-starsconf.synaptic.cl/graphql.')
 client = Client(transport=transport)
@@ -26,44 +27,27 @@ query = gql('''
 ''')
 
 response = client.execute(query)
-
-users = {}
+users = {
+    4624152: {
+        'alerttime': 5,
+        'active': True
+    },
+    274908043: {
+        'alerttime': 5,
+        'active': True
+    }
+}
 
 
 helpmsg = '''
 Current commands are:
 
-/alerttime 5    # Alert time in minutes
-/stop           # Stop alerts
-/restart        # Restart alerts
+/start          # start alerts
 /help           # show this help
 
 '''
 
-def helpme(chatid, *kw):
-    bot.sendMessage(
-        chatid,
-        helpmsg
-    )
-
-
-def alerttime(chatid, kw):
-    users[chatid][u'alerttime'] = int(kw[0])
-    bot.sendMessage(
-        chatid,
-        'Tiempo cambiado a {} minutos'.format(int(kw[0]))
-    )
-
-
-def stop(chatid, kw):
-    users[chatid][u'active'] = False
-    bot.sendMessage(
-        chatid,
-        'Alertas detenidas'
-    )
-
-
-def restart(chatid, kw):
+def start(chatid, kw):
     users[chatid][u'active'] = True
     bot.sendMessage(
         chatid,
@@ -71,10 +55,7 @@ def restart(chatid, kw):
     )
 
 dispatch = {
-    u'/alerttime': alerttime,
-    u'/restart': restart,
-    u'/stop': stop,
-    u'/help': helpme
+    u'/start': start
 }
 
 
@@ -82,6 +63,7 @@ def handle(msg):
     flavor = telepot.flavor(msg)
     summary = telepot.glance(msg, flavor=flavor)
     chatid = summary[2]
+    pprint(summary)
 
     # first time user
     if chatid not in users:
@@ -89,7 +71,6 @@ def handle(msg):
             'alerttime': 5,
             'active': True
         }
-        helpme(chatid)
 
     # dispatch commands
     if msg[u'text'][0] == u'/':
@@ -121,23 +102,26 @@ for talk in response[u'allTalks']:
         u'date': pendulum.parse(datestr, tz='America/Santiago'),
         u'users': []})
 
+pprint(talks)
+
 print 'Listening ...'
 while True:
     now = pendulum.now()
+    print users
     for chatid in users.keys():
         user = users[chatid]
         for talk in talks:
-            minutes_to_talk = (talk[u'date'] - now).total_seconds() / 60.
-
-            if (minutes_to_talk < user[u'alerttime'] and
-                    chatid not in talk[u'users']):
-                bot.sendMessage(
-                    chatid,
-                    '{} está a {} minutos de comenzar!'.format(
-                        talk[u'name'],
-                        user[u'alerttime']
+            if (talk[u'date'] - now).total_seconds() > 0:
+                minutes_to_talk = (talk[u'date'] - now).total_seconds() / 60.
+                if (minutes_to_talk < user[u'alerttime'] and
+                        chatid not in talk[u'users']):
+                    bot.sendMessage(
+                        chatid,
+                        '{} está a {} minutos de comenzar!'.format(
+                            talk[u'name'],
+                            user[u'alerttime']
+                        )
                     )
-                )
-                # user already alerted
-                talk[u'users'].append(chatid)
-    time.sleep(10)
+                    # user already alerted
+                    talk[u'users'].append(chatid)
+    time.sleep(2)
